02f7ce93bb4bb4116960333980f7a45ee0a05aa4
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-11-14 19:42:21 +0530

    Create onclick.js

---
+++ onclick.js
@@ -0,0 +1,13 @@
+import react from 'react'
+
+const But= () => {
+
+  return(
+    <div>
+      <button onClick={() => console.log("clicked")}>Click Me</button>
+    </div>
+  )
+
+}
+
+export default But
