a4871da514ec6e9abd382668a4e3fc9fbc4cefee
savarapu manohar joshi <83489094+Manohar-mj@users.noreply.github.com>
2022-11-15 12:03:55 +0530

    Create fetch.js

---
+++ fetch.js
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setData(json));
+  }, []);
+
+  return <div>
+
+    {
+      data.map(i=> <li k>{i.title}</li>)
+    }
+  </div>;
+}
+export default App;
